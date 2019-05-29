45586a2e9980119102c74d8f23edec1bd60ca808
jeniaka <37797998+jeniaka@users.noreply.github.com>
2019-05-29 23:23:08 +0300

    Update ass3.s

--- Assignment3/ass3.s
+++ Assignment3/ass3.s
@@ -14,23 +14,28 @@
     add esp,12
     %endmacro
 
+%macro zero_q 1
+    mov dword [%1],0
+    mov dword [%1+4],0
+%endmacro
 
-%macro create_random 1
-        push dword %1
-        call random_number 
-        add esp, 4
+%macro generate_num 2
+        push %1
+        push %2
+        call random_number
+        add esp,8
 %endmacro
 
 %macro allocateCo_routine 1
-	push STKSZ
-	call malloc                     ; Allocate stack size
-	add esp, 4
-	mov ebx, dword [CORS]
-	add ebx, edi
-	mov ebx, [ebx]
-	;add ebx, dword SPP
-	add eax, STKSZ 					; set in eax the address of the end of the stack
-	mov [ebx], %1
+    push STKSZ
+    call malloc                     ; Allocate stack size
+    add esp, 4
+    mov ebx, dword [CORS]
+    add ebx, edi
+    mov ebx, [ebx]
+    ;add ebx, dword SPP
+    add eax, STKSZ                  ; set in eax the address of the end of the stack
+    mov [ebx], %1
     mov dword [ebx + 4], eax        ; Set  cell in Cors array to point to  alocated stack
 %endmacro
 
@@ -50,6 +55,8 @@
         mov     ebx, dword[ebp + 8]
 %endmacro
 
+
+
 %macro endFunction 0
         popa
         mov     esp, ebp
@@ -66,20 +73,21 @@
 
 
 section .data
-    format_string_s : db "%s",0 
+    format_string_s : db "%s",10,0 
     format_string : db "%d",10,0 
     down :db '',10,0
     format_string_int: db "%d", 10, 0   ; format string int
     format_string_float: db "%f", 10, 0 ; format string float
-    format_string_floatl: db "%lf", 10, 0 ; format string float
+    format_string_floatl: db "%1f", 10, 0 ; format string float
     format_string_2f: db "%.2f",10,0 ; float 2 numbers after dot
     degree equ 360
     distance equ 100
     maxint: dd 0xffff
-    bignum: DD 0
+    arg1: DD 0
+    arg2: DD 0
     res : dd 0
-	struct_len equ 8
-	SPP equ 4 							; offset of pointer to co-routine stack in co-routine struct 
+    struct_len equ 8
+    SPP equ 4                           ; offset of pointer to co-routine stack in co-routine struct 
 
 
 
@@ -94,10 +102,10 @@ section .bss
 
     ;------------Co-routines fields------------;
     CURR: resd 1
-    SPT: resd 1 						; temporary stack pointer
-    SPMAIN: resd 1						; stack pointer of main
-    STKSZ equ 16*1024					; co-routine stack size
-    CODEP equ 0 						; offset of pointer to co-routine function in co-routine struct 
+    SPT: resd 1                         ; temporary stack pointer
+    SPMAIN: resd 1                      ; stack pointer of main
+    STKSZ equ 16*1024                   ; co-routine stack size
+    CODEP equ 0                         ; offset of pointer to co-routine function in co-routine struct 
     
     
 
@@ -106,12 +114,13 @@ section .bss
 
 section .text                           ; functions from c libary
   align 16
+     global res
      global main 
      global random_number
-	 extern drone
-	 extern target
-	 extern scheduler
-	 extern printer
+     extern drone
+     extern target
+     extern scheduler
+     extern printer
      extern printf 
      extern fprintf
      extern sscanf
@@ -158,7 +167,7 @@ main:
         je endAlcCoRou
         pushad
 
-		shl ecx, 2 						; multiply by 4
+        shl ecx, 2                      ; multiply by 4
         push ecx                        ; allocate array of (4*N) bytes
         call malloc
         add esp, 4
@@ -167,78 +176,80 @@ main:
 
         xor edi, edi
         xor ebx, ebx
-		mov edi, [CORS]
+        mov edi, [CORS]
 
-		allocStructsLoop:
-			pushad
-			push struct_len 			; malloc with 8 bytes
-			call malloc
-			add esp, 4 			
-			mov [edi + ebx], eax		; in the i'th cell of CORS array put the new allocated address
-			popad
-			add ebx, dword 4
-		loop allocStructsLoop, ecx
+        allocStructsLoop:
+            pushad
+            push struct_len             ; malloc with 8 bytes
+            call malloc
+            add esp, 4          
+            mov [edi + ebx], eax        ; in the i th cell of CORS array put the new allocated address
+            popad
+            add ebx, dword 4
+        loop allocStructsLoop, ecx
 
 
-		xor edi, edi
-		mov ecx, dword [N]
+        xor edi, edi
+        mov ecx, dword [N]
         allocDroneLoop:
             pushad
-            allocateCo_routine dword drone 	; macro to create drone co-routine
+           ; allocateCo_routine dword drone  ; macro to create drone co-routine
             popad
             add edi, dword 4
         loop allocDroneLoop, ecx
-		
-		xor ebx, ebx
-		allocScheduler:
-			pushad
-			allocateCo_routine dword scheduler 	; macro to create scheduler co-routine
-			popad
-		add edi, dword 4
-		
-		allocTarget:
-			pushad
-			allocateCo_routine dword target 	; macro to create target co-routine
-			popad
-		add edi, dword 4
-		
-		allocPrinter:
-			pushad
-			allocateCo_routine dword printer 	; macro to create printer co-routine
-			popad
-		
+        
+        xor ebx, ebx
+        allocScheduler:
+            pushad
+          ;  allocateCo_routine dword scheduler  ; macro to create scheduler co-routine
+            popad
+        add edi, dword 4
+        
+        allocTarget:
+            pushad
+          ;  allocateCo_routine dword target     ; macro to create target co-routine
+            popad
+        add edi, dword 4
+        
+        allocPrinter:
+            pushad
+           ; allocateCo_routine dword printer    ; macro to create printer co-routine
+            popad
+        
         endAlcCoRou:
 
-		xor ecx, ecx
-		xor edi, edi
-		mov ecx, dword [N]
-		add ecx, dword 3
-		
-		initCoLoop:
-			pushad
-			push edi
-			call initCo
-			add esp, 4
-			popad
-			inc edi
-		loop initCoLoop, ecx
+        xor ecx, ecx
+        xor edi, edi
+        mov ecx, dword [N]
+        add ecx, dword 3
+        
+        initCoLoop:
+            pushad
+            push edi
+            call initCo
+            add esp, 4
+            popad
+            inc edi
+        loop initCoLoop, ecx
 
 
 
-        create_random distance
+
+        mov ebx,50
+        create_random distance ,dword [ebx]
         print_float
-        create_random degree
-        print_float
+        ;create_random degree
+       ; print_float
         jmp endAss3
     
     ;----------initCo Function---------;
     initCo:
         startFunction               ; get co-routine ID number
-		mov edx, dword[CORS]
+        mov edx, dword[CORS]
         mov ebx, [4*ebx + edx]      ; get pointer to COi struct
         mov eax, [ebx+CODEP]        ; get initial EIP value – pointer to COi function
         mov [SPT], esp              ; save ESP value
-		mov esi, dword [ebx + SPP]
+        mov esi, dword [ebx + SPP]
         mov esp, [ebx+SPP]          ; get initial ESP value – pointer to COi stack
         push eax                    ; push initial “return” address
         pushfd                      ; push flags
@@ -250,10 +261,10 @@ main:
     
     
    
-    random_number:
-        startFunction
-        mov dword [bignum],ebx
-
+   random_number:
+        push    ebp
+        mov     ebp, esp
+        sub esp,4
  LFSR:  
         mov edi,16
         looplfsr:
@@ -285,12 +296,16 @@ main:
 
     scaling:
         finit
-        fild dword [bignum]
+        fild dword [ebp+8]
+        fild dword [ebp+12]
+        fsub
         fild dword [maxint]
         fdiv
         fild dword [seed]
         fmul
-        fstp qword [res] 
+        fild dword [ebp+12]
+        fadd
+        fstp qword [res]
     endFunction
 
     
