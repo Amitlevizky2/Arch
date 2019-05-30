12e9d1dc51a0fd9bebd4bdc6e447c0297f041c9e
Amitlevizky2 <37595736+Amitlevizky2@users.noreply.github.com>
2019-05-31 01:00:56 +0300

    Add files via upload

--- Assignment3/ass3.s
+++ Assignment3/ass3.s
@@ -5,13 +5,15 @@
     add esp, 8
 %endmacro
 
-%macro print_float 0
-    fld qword [res]
+%macro print_float 1
+    pushad
+    fld qword %1
     sub esp,8
     fstp qword [esp]
     push format_string_2f
     call printf
     add esp,12
+    popad
     %endmacro
 
 
@@ -30,8 +32,24 @@
 	mov ebx, [ebx]
 	;add ebx, dword SPP
 	add eax, STKSZ 					; set in eax the address of the end of the stack
+	mov esi, dword %1
 	mov [ebx], %1
     mov dword [ebx + 4], eax        ; Set  cell in Cors array to point to  alocated stack
+%endmacro
+
+%macro allocateCo_routine_drone 2
+	push STKSZ
+	call malloc                     ; Allocate stack size
+	add esp, 4
+	mov ebx, dword [CORS]
+	add ebx, edi
+	mov ebx, [ebx]
+	;add ebx, dword SPP
+	add eax, STKSZ 					; set in eax the address of the end of the stack
+	mov esi, dword %1
+	mov [ebx], %1
+    mov dword [ebx + 4], eax        ; Set  cell in Cors array to point to  alocated stack
+    mov dword [ebx + 8], %2
 %endmacro
 
 %macro callscanf 3
@@ -51,7 +69,6 @@
 %endmacro
 
 %macro endFunction 0
-        popa
         mov     esp, ebp
         pop     ebp
         ret
@@ -64,6 +81,17 @@
         ret
 %endmacro
 
+%macro zero_q 1
+    mov dword [%1],0
+    mov dword [%1+4],0
+%endmacro
+
+%macro generate_num 2
+        push %1
+        push %2
+        call random_number
+        add esp,8
+%endmacro
 
 section .data
     format_string_s : db "%s",0 
@@ -78,29 +106,9 @@ section .data
     maxint: dd 0xffff
     bignum: DD 0
     res : dd 0
-	struct_len equ 8
+	struct_len equ 12
+    drone_struct_len equ 28
 	SPP equ 4 							; offset of pointer to co-routine stack in co-routine struct 
-
-
-
-section .bss
-    N : resd 1                          ; Number of drones
-    T : resd 1                          ; Number of targets to destroy to win the game
-    K : resd 1                          ; How many drone steps between game board printings
-    beta : rest 1                       ; Angle of drone field-of-view
-    d : rest 1                          ; Maximum distance that allows to destroy a target
-    seed : resd 1                       ; Seed for initialization of LFSR shift register
-    CORS : resd 1                       ; Number of all the co-routines in the program
-
-    ;------------Co-routines fields------------;
-    CURR: resd 1
-    SPT: resd 1 						; temporary stack pointer
-    SPMAIN: resd 1						; stack pointer of main
-    STKSZ equ 16*1024					; co-routine stack size
-    CODEP equ 0 						; offset of pointer to co-routine function in co-routine struct 
-    
-    
-
 
 
 
@@ -108,6 +116,21 @@ section .text                           ; functions from c libary
   align 16
      global main 
      global random_number
+     global N
+     global T
+     global K
+     global seed
+     global beta
+     global d
+     global AlcCoRoutins
+     global preInitCoLoop
+     global startCo
+     global CORS
+     global dronesArray
+     global schedulerCo
+     global targetCo
+     global printerCo
+     global resume
 	 extern drone
 	 extern target
 	 extern scheduler
@@ -118,9 +141,7 @@ section .text                           ; functions from c libary
      extern malloc
      extern free
 main:
-
     mov eax, dword [esp + 8]
-
     getArgsValues:
         pushad
         callscanf N, format_string_int, dword [eax + 4]     ; Number of drones
@@ -145,6 +166,11 @@ main:
         pushad
         callscanf seed, format_string_int, dword [eax + 24] ; Seed for initialization of LFSR shift register
         popad
+	call AlcCoRoutins
+    call initDronesArray
+	call preInitCoLoop
+	call startCo
+	jmp endAss3
 
             
      
@@ -178,15 +204,30 @@ main:
 			popad
 			add ebx, dword 4
 		loop allocStructsLoop, ecx
-
+		
+		saveCoNumbers:
+            mov esi, dword [N]
+            inc esi
+            shl esi, 2
+            mov dword [schedulerCo], esi	; save the co-routine number of scheduler
+            shr esi, 2
+            inc esi
+            shl esi, 2
+            mov dword [targetCo], esi		; save the co-routine number of target
+            shr esi, 2
+            inc esi
+            shl esi, 2
+            mov dword [printerCo], esi		; save the co-routine number of printer
 
 		xor edi, edi
+        xor esi, esi
 		mov ecx, dword [N]
         allocDroneLoop:
             pushad
-            allocateCo_routine dword drone 	; macro to create drone co-routine
+            allocateCo_routine_drone dword drone, dword esi 	; macro to create drone co-routine
             popad
             add edi, dword 4
+            add esi, dword 1
         loop allocDroneLoop, ecx
 		
 		xor ebx, ebx
@@ -208,28 +249,90 @@ main:
 			popad
 		
         endAlcCoRou:
+		ret
 
-		xor ecx, ecx
-		xor edi, edi
-		mov ecx, dword [N]
-		add ecx, dword 3
+	
+    initDronesArray:
+        preInitDrone:
+        mov ecx, dword [N]                          ; Number of drones
+        pushad
+        shl ecx, 2                                  ; N*4 to compute the size of the array
+        push ecx
+        call malloc
+        add esp, 4
+        mov dword [dronesArray], eax                ; Set dronesArray to point to the array that was allocated
+        popad
+
+        xor esi, esi
+        initDroneLoop:
+        cmp ecx, dword 0
+        je endDroneInit
+        mov ebx, dword [dronesArray]                ; The array
+        add ebx, esi                                ; the right drone index
+        push drone_struct_len                       ; size of struct for each drone
+        pushad
+        call malloc
+        popad
+        add esp, 4
+        mov dword [ebx], eax                        ; set pointer to the new allocated space
+        
+        initRandomDroneValues:
+        xor edx, edx
+        pushad
+        generate_num edx, distance                  ; Get X random value
+        popad
+        fld qword [res]                             ; load res to the first cell in stack
+        fstp qword [ebx + xPlace]                   ; put it at the place ebx point at
+        ;print_float qword [ebx + xPlace]
+
+        xor edx, edx
+        pushad
+        generate_num edx, distance                  ; Get Y random value
+        popad
+        fld qword [res]                             ; load res to the first cell in stack
+        fstp qword [ebx + yPlace]                   ; put it at the place ebx point at
+        ;print_float qword [ebx + yPlace]
+
+        xor edx, edx
+        pushad
+        generate_num edx, degree                    ; Get alpha random value
+        popad
+        fld qword [res]                             ; load res to the first cell in stack
+        fstp qword [ebx + alphaPlace]               ; put it at the place ebx point at
+        ;print_float qword [ebx + alphaPlace]
+
+        mov dword [ebx + targetsPlace], 0           ; put targets destroyed to be 0
+        add esi, dword 4
+        dec ecx
+        jmp initDroneLoop
+        endDroneInit:
+        ret
+
+        
+
+    preInitCoLoop:
+	xor ecx, ecx
+	xor edi, edi
+	mov ecx, dword [N]
+	add ecx, dword 3
 		
-		initCoLoop:
-			pushad
-			push edi
-			call initCo
-			add esp, 4
-			popad
-			inc edi
-		loop initCoLoop, ecx
+	initCoLoop:
+		pushad
+		push edi
+		call initCo 			; for every co-routine performe a initialization
+		add esp, 4
+		popad
+		inc edi
+	loop initCoLoop, ecx
+	ret
 
-
-
-        create_random distance
-        print_float
-        create_random degree
-        print_float
-        jmp endAss3
+    mov ebx,50
+    generate_num ebx ,distance
+    ;print_float
+    mov edx ,0
+	;generate_num  degree
+    ;print_float
+    jmp endAss3
     
     ;----------initCo Function---------;
     initCo:
@@ -248,12 +351,41 @@ main:
         endFunction
     ;----------end initCo Function---------;
     
-    
-   
-    random_number:
-        startFunction
-        mov dword [bignum],ebx
+	;----------initCo Function---------;
+    startCo:
+		pushad 							; save registers of main ()
+		mov [SPMAIN], esp 				; save ESP of main ()
+		mov ebx, dword [CORS] 			; gets ID of a scheduler co-routine
+		add ebx, dword [schedulerCo] 	; gets a pointer to a scheduler struct
+				;sub ebx, dword 4
+				mov esi, [ebx]
+				mov esi, [esi]
+				mov ebx, [ebx]
+		jmp do_resume 					; resume a scheduler co-routine
 
+	endCo:
+		mov esp, [SPMAIN] 				; restore ESP of main()
+		popad							; restore registers of main()
+		ret
+
+	resume:  							; save state of current co-routine
+		pushfd
+		pushad
+		mov edx, [CURR]
+		mov [edx+SPP], esp 				; save current ESP
+    
+	do_resume: 							; load ESP for resumed co-routine
+		mov esp, [ebx + SPP]
+		mov [CURR], ebx
+		popad
+		popfd
+		ret
+	
+	;----------random_number Function---------;
+     random_number:
+        push    ebp
+        mov     ebp, esp
+        sub esp,4
  LFSR:  
         mov edi,16
         looplfsr:
@@ -285,15 +417,52 @@ main:
 
     scaling:
         finit
-        fild dword [bignum]
+        fild dword [ebp+8]
+        fild dword [ebp+12]
+        fsub
         fild dword [maxint]
         fdiv
         fild dword [seed]
         fmul
-        fstp qword [res] 
+        fild dword [ebp+12]
+        fadd
+        fstp qword [res]
     endFunction
-
+	;----------end random_number Function---------;
     
+	freeMemoryBeforeExit:
+
 
 
 endAss3:
+
+
+section .bss
+    N : resd 1                          ; Number of drones
+    T : resd 1                          ; Number of targets to destroy to win the game
+    K : resd 1                          ; How many drone steps between game board printings
+    beta : rest 1                       ; Angle of drone field-of-view
+    d : rest 1                          ; Maximum distance that allows to destroy a target
+    seed : resd 1                       ; Seed for initialization of LFSR shift register
+    CORS : resd 1                       ; Number of all the co-routines in the program
+    dronesArray : resd 1                ; Array to keep each drone details
+	schedulerCo : resd 1 				; Pointer to scheduler co-routine
+	targetCo : resd 1 					; Pointer to target co-routine
+	printerCo : resd 1					; Pointer to printer co-routine
+    xValue : resq 1
+    yValue : resq 1
+    alphValue : resq 1
+    struc drone_struc
+        xPlace : resq 1
+        yPlace : resq 1
+        alphaPlace : resq 1
+        targetsPlace : resd 1
+    endstruc
+
+    ;------------Co-routines fields------------;
+    CURR: resd 1
+    SPT: resd 1 						; temporary stack pointer
+    SPMAIN: resd 1						; stack pointer of main
+    STKSZ equ 16*1024					; co-routine stack size
+    CODEP equ 0 						; offset of pointer to co-routine function in co-routine struct 
+    
