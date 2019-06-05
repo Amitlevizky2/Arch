25a321090369b9d9d51d22cfe35212a0f503b1ad
Amitlevizky2 <37595736+Amitlevizky2@users.noreply.github.com>
2019-06-05 11:41:56 +0300

    Add files via upload

--- Assignment3/scheduler.s
+++ Assignment3/scheduler.s
@@ -1,4 +1,41 @@
+
+
+section .data
+  	SPP equ 4 							; offset of pointer to co-routine stack in co-routine struct 
 section .text                           ; functions from c libary
   align 16
 	 global scheduler:function
+   extern resume
+   extern drone
+   extern printer
+   extern main
+   extern printerCo
+   extern CORS
+   extern K
+    
      scheduler:
+    xor edi, edi
+    xor esi, esi
+    xor eax, eax
+    mov edi, dword 0
+    schedulerLoop:
+      cmp edi, [K]
+      je gotoPrinter
+      mov eax, [CORS]
+      add eax, esi
+      mov ebx, [eax]
+      ;mov ebx, [eax + SPP]
+      call resume
+      add esi, dword 4
+      jmp schedulerLoop
+    
+    gotoPrinter:
+      mov eax, [CORS]
+      add eax, [printerCo]
+      mov eax, [eax]
+      mov ebx, [eax + SPP]
+      call resume
+      xor edi, edi
+      jmp schedulerLoop
+
+
